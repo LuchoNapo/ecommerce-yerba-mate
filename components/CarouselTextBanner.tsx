7397a3f15c2be11078a7f17fd20c167e2245167d
Lucho Napo <Luciano_napo@hotmail.com>
2024-11-08 15:03:38 -0300

    [fix] featureProducts & skeleton

--- components/CarouselTextBanner.tsx
+++ components/CarouselTextBanner.tsx
@@ -1,5 +1,4 @@
 "use client"
-import { useRouter } from "next/navigation";
 import { Carousel, CarouselContent, CarouselItem } from "./ui/carousel";
 import { Card, CardContent } from "./ui/card";
 import Autoplay from "embla-carousel-autoplay";
@@ -9,31 +8,27 @@ export const dataCarouselTop = [
         id: 1,
         title: "Envio en 24/48 hs",
         description: "Como cliente VIP, tus envios en 24/48 horas. Obtén más información y unete!",
-        link: "#!"
     },
     {
         id: 2,
         title: "Consigue hasta un -25% en compras superiores a $40.000",
         description: "-20% al gastar $100.000 o -25% al gastar $150.000. Usa el código MATEICO ",
-        link: "#!"
     },
     {
         id: 3,
         title: "Cuotas sin interes ",
         description: "Gozá hasta 6 cuotas sin interes.",
-        link: "#!"
     },
     {
         id: 4,
         title: "Envío gratuito",
         description: "Envio gratuito a partir de compras superiores a $50.000",
-        link: "#!"
     }
 ]
 
 
 const CarouselTextBanner = () => {
-    const router = useRouter()
+
     return (
         <div className="bg-gray-100 dark:bg-neutral-900">
             <Carousel className="w-full max-w-4xl mx-auto" plugins={[
@@ -45,11 +40,8 @@ const CarouselTextBanner = () => {
                     loop: true,
                 }}>
                 <CarouselContent>
-
-
-
-                    {dataCarouselTop.map(({ id, title, description, link }) => (
-                        <CarouselItem key={id} onClick={() => router.push(link)} className="cursor-pointer flex justify-center items-center text-pretty">
+                    {dataCarouselTop.map(({ id, title, description }) => (
+                        <CarouselItem key={id} className="select-none flex justify-center items-center text-pretty">
                             <div>
                                 <Card className="shadow-none border-none bg-transparent">
                                     <CardContent className="flex flex-col justify-center p-2 items-center text-center ">
--- components/FeaturedProducts.tsx
+++ components/FeaturedProducts.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 "use client"
 
+import { useState, useEffect } from "react";
 import { useGetFeaturedProducts } from "@/api/getFeaturedProducts";
 import { ResponseType } from "@/types/response";
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "./ui/carousel";
@@ -17,58 +18,62 @@ const FeaturedProducts = () => {
     const { loading, result }: ResponseType = useGetFeaturedProducts()
     const router = useRouter()
     const { addItem } = useCart()
-
-
+    const [grid, setGrid] = useState(0)
+    useEffect(() => {
+      const updateGrid = () => {
+        setGrid(window.innerWidth < 768 ? 1 : 3);
+      };
+      updateGrid();
+     
+    }, [])
+    
     return (
         <div className="max-w-6xl py-4 mx-auto sm:py-16 sm:pb-28 sm:px-14">
-            <h3 className="px-6 text-2xl sm:text-3xl sm:pb-8">Productos destacados</h3>
+            <h3 className="px-6 text-2xl sm:text-3xl pb-4">Productos destacados</h3>
             <Carousel
                 opts={{
                     loop: true,
                 }}
             >
-                <CarouselContent className="select-none">
-                    {loading && <SkeletonSchema grid={3} />}
+                <CarouselContent className="select-none mx-4">
+                    {loading && <SkeletonSchema grid={grid} />}
                     {result != null && (
                         result.map((product: ProductType) => {
                             return (
                                 <CarouselItem key={product.id} className="md:basis-1/2 lg:basis-1/3 group">
-                                    <div className="p-1">
-                                        <Card className="py-4 border bg-gray-100 dark:bg-[#0D0B0A] dark:border-stone-900 border-gray-200 shadow-none">
-                                            <CardContent className="relative flex items-center justify-center px-6 py-2">
-                                                <img
-                                                    className="size-[250px] object-contain select-none"
-                                                    src={`${product.images[0].url}`}
-                                                    alt={product.productName}
-                                                />
-                                                
+                                    <Card className="border bg-gray-100 dark:bg-[#0D0B0A] dark:border-stone-900 border-gray-200 shadow-none">
+                                        <CardContent className="relative flex items-center justify-center py-2">
+                                            <img
+                                                className="size-[200px] object-contain select-none"
+                                                src={`${product.images[0].url}`}
+                                                alt={product.productName}
+                                            />
 
-                                                <div className="absolute w-full px-6 transition duration-200 opacity-0 group-hover:opacity-100 bottom-5">
-                                                    <div className="flex justify-center gap-x-6">
-                                                        <IconButton
-                                                            onClick={() => router.push(`/product/${product.slug}`)}
-                                                            icon={<Expand size={20} />}
-                                                            className="text-gray-600"
-                                                        />
-                                                        <IconButton
-                                                            onClick={() => addItem(product)}
-                                                            icon={<ShoppingCart size={20} />}
-                                                            className="text-gray-600"
-                                                        />
-                                                    </div>
+
+                                            <div className="absolute w-full px-6 transition duration-200 opacity-0 group-hover:opacity-100 bottom-5">
+                                                <div className="flex justify-center gap-x-6">
+                                                    <IconButton
+                                                        onClick={() => router.push(`/product/${product.slug}`)}
+                                                        icon={<Expand size={20} />}
+                                                        className="text-gray-600"
+                                                    />
+                                                    <IconButton
+                                                        onClick={() => addItem(product)}
+                                                        icon={<ShoppingCart size={20} />}
+                                                        className="text-gray-600"
+                                                    />
                                                 </div>
-                                            </CardContent>
-                                            <div className="flex flex-col justify-between gap-4 px-8 text-center">
-                                                <h3 title={product.productName} className="text-lg font-bold truncate text-black dark:text-white" >{product.productName}</h3>
-                                                <Badge product={product} class="text-sm justify-center" />
                                             </div>
-                                        </Card>
-                                    </div>
+                                        </CardContent>
+                                        <div className="flex flex-col justify-between  px-8 text-center">
+                                            <h3 title={product.productName} className="text-lg font-bold truncate text-black dark:text-white" >{product.productName}</h3>
+                                            <Badge product={product} class="text-sm justify-center" />
+                                        </div>
+                                    </Card>
                                 </CarouselItem>
                             )
                         })
                     )}
-
                 </CarouselContent>
                 <CarouselPrevious />
                 <CarouselNext className="hidden sm:flex" />
